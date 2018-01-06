5a8170b7a893d39a54fa23f32892dc4410152a38
ianbaremans <ianbaremans@gmail.com>
2018-01-06 17:09:08 +0100

    Prints the black "screen"

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
 int main () {
-    std::cout << "\033[48;2;123;12;233m\u2584\033[0m\n";
-    return 0;
+    for (int i = 0; i < 5; ++i) {
+        for (int i = 0; i < 60; ++i) {
+            const std::string color("\033[48;2;0;0;0;38;2;0;0;0m");
+            const std::string reset("\033[0m");
+            std::cout << color << "\u2584" << reset;
+        }
+        std::cout << "\n";
+    }
 }
 
