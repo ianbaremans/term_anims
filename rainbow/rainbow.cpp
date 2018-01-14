6bb18a00a7017605e59b4901f0b4eebb9255d4ef
ianbaremans <ianbaremans@gmail.com>
2018-01-14 15:40:57 +0100

    Initial commit for rainbow animation

---
+++ rainbow/rainbow.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void rainbow () {
+    for (int i = 0; i < 5; ++i) {
+        for (int i = 0; i < 60; ++i) {
+        }
+        std::cout << "\n";
+    }
+
+}
+
+int main () {
+    int i = 1;
+    while (i = 1) {
+        rainbow();
+        usleep(10000);
+}
+
